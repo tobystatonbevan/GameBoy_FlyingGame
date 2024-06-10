0048a629cb6613f6e12768cb95454af51bafd6c1
tobystatonbevan <154239390+tobystatonbevan@users.noreply.github.com>
2024-06-10 18:30:33 +0100

    added background
    changed movement method

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "arrow.c"
+#include "arrowmap.c"
+#include "citymaptiles.c"
 
 void main(void)
 {
+    //background
+    set_bkg_data(0,10,citymaptextures);
+    set_bkg_tiles(0,0,40,36,citymap);
+    SHOW_BKG;
+    DISPLAY_ON;
+    
+    //sprites
     UINT8 currentspriteindex = 0;
     int counter = 0;
     set_sprite_data(0,4,Arrow);
@@ -42,22 +51,26 @@ void main(void)
             case J_LEFT:
                 currentspriteindex = 2;
                 set_sprite_tile(0, currentspriteindex);
-                scroll_sprite(0,-10,0);
+                //scroll_sprite(0,-10,0);
+                scroll_bkg(-8,0);
                 break;
             case J_RIGHT:
                 currentspriteindex = 3;
                 set_sprite_tile(0, currentspriteindex);
-                scroll_sprite(0,10,0);
+                //scroll_sprite(0,10,0);
+                scroll_bkg(8,0);
                 break;
             case J_DOWN:
                 currentspriteindex = 1;
                 set_sprite_tile(0, currentspriteindex);
-                scroll_sprite(0,0,10);
+                //scroll_sprite(0,0,10);
+                scroll_bkg(0,8);
                 break;
             case J_UP:
                 currentspriteindex = 0;
                 set_sprite_tile(0, currentspriteindex);
-                scroll_sprite(0,0,-10);
+                //scroll_sprite(0,0,-10);
+                scroll_bkg(0,-8);
                 break;
         } 
         delay(100);  
